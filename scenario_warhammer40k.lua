8371df23d4be7097019ca0cec50c6b8794808528
Thomas Salmon <th_s@hotmail.fr>
2020-02-04 01:02:55 +0100

    init map

---
+++ scenario_warhammer40k.lua
@@ -0,0 +1,11 @@
+-- Name: Warhammer 40k
+-- Description: Map of the roleplay game.
+--- WIP
+-- Type: Mission
+
+function init()
+    player = PlayerSpaceship():setFaction("Human Navy"):setTemplate("Atlantis")
+end
+
+function update(delta)
+end
